5631a1b98be8a4322a94db5b42ae1bdeba2a7691
jozwright <jozwright@gmail.com>
2014-08-13 12:19:56 -0700

    CRUD functions working

--- lib/cd.rb
+++ lib/cd.rb
@@ -1,10 +1,22 @@
 class Cd
 
 	attr_reader :album, :artist
+	@@all_cds = []
 
 	def initialize(album, artist)
 		@album = album
 		@artist = artist
 	end
 
+	def save 
+		@@all_cds << self
+	end
+
+	def self.all
+		@@all_cds
+	end
+
+	def self.clear
+		@@all_cds = []
+	end
 end
--- spec/cd_spec.rb
+++ spec/cd_spec.rb
@@ -2,8 +2,24 @@ require 'rspec'
 require 'cd'
 
 describe Cd do
+	before do
+		Cd.clear
+	end
+
 	it 'is initialized with an album and artist name' do
 		test_cd = Cd.new 'In Utero', 'Nirvana'
 		test_cd.should be_a Cd
+	end
+
+	it 'allows you to add multiple cds' do
+		test_cd = Cd.new 'Universal Mother', "Sinead O'Connor"
+		test_cd.save
+		test_cd_two = Cd.new 'In Utero', 'Nirvana'
+		test_cd_two.save
+		Cd.all.should eq [test_cd, test_cd_two]
+	end
+
+	it 'clears all contacts' do
+		Cd.clear.should eq []
 	end
 end
